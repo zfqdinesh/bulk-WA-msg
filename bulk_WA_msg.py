41b0492ff990aec2b2e4ecb452447bf38345cfae
zfqdinesh <mynamedineshajmera@gmail.com>
2024-12-01 23:13:12 +0530

    Update bulk_WA_msg.py
    add pdf and images to the massage

--- bulk_WA_msg.py
+++ bulk_WA_msg.py
@@ -14,9 +14,11 @@ send_msg_time = 10               # Time for sending a message (in seconds)
 country_code = 91               # Set your country code
 action_time = 2                 # Set time for button click action
 image_path = ("C:/Users/Dell/OneDrive/Pictures/1.jpg",
-              "C:/Users/Dell/OneDrive/Pictures/1.jpg",
               "C:/Users/Dell/OneDrive/Pictures/1.jpg"
  ) # Absolute path to you image
+
+pdf_path="C:/Users/Dell/OneDrive/Desktop/Downloads/23EAOCA025.pdf"
+ 
 
 # Create driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -46,17 +48,31 @@ with open('numbers.txt', 'r') as file:
             attach_btn = driver.find_element(By.CSS_SELECTOR, "div.x78zum5.x6s0dn4 > button[title='Attach']")
             attach_btn.click()
             time.sleep(action_time)
-        # Find and send image path to input
+            # Find and send image path to input
         
         
             msg_input = driver.find_element(By.CSS_SELECTOR, "div.x1i64zmx.x1emribx input")
             msg_input.send_keys(image)
             time.sleep(action_time)
-        # Start the action chain to write the message
+            # Start the action chain to write the message
             actions = ActionChains(driver)
             actions.send_keys(Keys.ENTER)
             actions.perform()
             time.sleep(send_msg_time)
+        
+        
+        attach_btn = driver.find_element(By.CSS_SELECTOR, "div.x78zum5.x6s0dn4 > button[title='Attach']")
+        attach_btn.click()
+        time.sleep(action_time)
+        pdf_input = driver.find_element(By.CSS_SELECTOR, "li._aj-r._aj-q._aj-_._ak_v input[type='file']")
+        pdf_input.send_keys(pdf_path)
+        time.sleep(action_time)
+            
+        actions = ActionChains(driver)  # Start the action chain to write the message
+        actions.send_keys(Keys.ENTER)
+        actions.perform()
+        time.sleep(send_msg_time)
+        
 
 # Quit the driver
 driver.quit()
